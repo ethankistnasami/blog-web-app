80d5965170f613af8d6524262c82e06467aacece
ethankistnasami <ekistnasami@gmail.com>
2026-01-28 22:14:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ const saltRounds = 10;
 env.config();                     
 
 const db = new Pool({
-    user: process.env.PG_USER,
-    database: process.env.PG_DATABASE,
-    host: process.env.PG_HOST,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT
-    // connectionString: process.env.DATABASE_URL
+    // user: process.env.PG_USER,
+    // database: process.env.PG_DATABASE,
+    // host: process.env.PG_HOST,
+    // password: process.env.PG_PASSWORD,
+    // port: process.env.PG_PORT
+    connectionString: process.env.DATABASE_URL
     
 });
 
